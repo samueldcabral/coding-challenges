35f7b280def18ede6d0d325c22480704d0fb1165
Samuel Deschamps Cabral <samueldcabral@gmail.com>
2023-09-08 20:13:59 -0300

    deleted all old coding challenges after separating them into a branch

--- Old coding challenges/02) Kata - first-factorial/main.js
+++
@@ -1,9 +0,0 @@
-// Challenge
-// Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
-
-function FirstFactorial(num){
-  return num === 0 ? 1 : num * FirstFactorial(num -1 );
-}
-
-console.log(FirstFactorial(4));
-console.log(FirstFactorial(8))
--- Old coding challenges/05) Kata - Simple Adding/main.js
+++
@@ -1,9 +0,0 @@
-// Using the JavaScript language, have the function SimpleAdding(num) add up all the numbers from 1 to num. For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000. 
-
-
-function SimpleAdding(num) { 
-  // code goes here 
-  return num > 0 ? num += SimpleAdding(num - 1) : 0;
-}
-console.log(SimpleAdding(140));
-console.log(SimpleAdding(12));
